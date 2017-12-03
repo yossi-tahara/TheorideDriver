e8a1ae3b35ffd034749457ed77505861b7e4ce87
yossi-tahara <yossi-tahara@theolizer.com>
2017-12-03 10:55:49 +0900

    リプレース機能復活

---
+++ replace.bat
@@ -0,0 +1,18 @@
+@echo off
+
+openfiles > NUL 2>&1
+if NOT %ERRORLEVEL% EQU 0 (
+    echo ä«óùé“å†å¿Ç≈é¿çsÇµÇƒâ∫Ç≥Ç¢ÅB
+    goto End 
+)
+
+set COMPILER_PATH=
+for /f "usebackq delims=" %%a in (`where /F cl.exe`) do set COMPILER_PATH=%%a
+echo %COMPILER_PATH%
+
+call impl.bat %COMPILER_PATH% replace HostX64\x64
+call impl.bat %COMPILER_PATH% replace HostX64\x86
+call impl.bat %COMPILER_PATH% replace HostX86\x64
+call impl.bat %COMPILER_PATH% replace HostX86\x86
+
+:End
--- zz_command.bat
+++ zz_command.bat
@@ -1,1 +1,4 @@
+%~d0
+cd  %~dp0
+
 cmd.exe /K "N:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
