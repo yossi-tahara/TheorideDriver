8d10a8426b1dc1ff1a73621dfe2a8a8e4657e5ad
yossi-tahara <yossi-tahara@theolizer.com>
2017-12-05 01:02:30 +0900

    細々調整した

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,9 @@
 #define THEORIDE_DO_PROCESS
 
+//----------------------------------------------------------------------------
+//      基本型の出力用
+//----------------------------------------------------------------------------
+
 #include <iostream>
 #include <sstream>
 
@@ -9,8 +13,9 @@ std::string getString(tType iValue , unsigned=0)
     return std::to_string(iValue);
 }
 
-constexpr char const*   kStr="abc";
-const char kStr2[]="def";
+//----------------------------------------------------------------------------
+//      enum型
+//----------------------------------------------------------------------------
 
 enum class EnumTest
 {
@@ -19,6 +24,7 @@ enum class EnumTest
     Symbol2
 };
 
+// 自動生成指示
 void GenerationMarkerStart(EnumTest, char const* inc="enum.inc");
 #define THEORIDE_ENUM EnumTest
 #define THEORIDE_ENUM_LIST()\
@@ -30,55 +36,78 @@ void GenerationMarkerStart(EnumTest, char const* inc="enum.inc");
 #undef THEORIDE_ENUM
 void GenerationMarkerEnd(EnumTest);
 
+//----------------------------------------------------------------------------
+//      基底クラス
+//----------------------------------------------------------------------------
+
 class PublicBase
 {
     unsigned long   mULong;
 public:
     PublicBase(unsigned long iULong) : mULong(iULong) { }
+
+    // 自動生成指示
     void GenerationMarkerStart(PublicBase, char const* inc="intrusive.inc");
-#define THEORIDE_CLASS PublicBase
-#define THEORIDE_BASE_LIST()
-#define THEORIDE_ELEMENT_LIST()\
-    THEORIDE_ELEMENT_PRIVATE(mULong)
-#include "intrusive.inc"
-#undef THEORIDE_ELEMENT_LIST
-#undef THEORIDE_BASE_LIST
-#undef THEORIDE_CLASS
-void GenerationMarkerEnd(PublicBase);
+    #define THEORIDE_CLASS PublicBase
+    #define THEORIDE_BASE_LIST()
+    #define THEORIDE_ELEMENT_LIST()\
+        THEORIDE_ELEMENT_PRIVATE(mULong)
+    #include "intrusive.inc"
+    #undef THEORIDE_ELEMENT_LIST
+    #undef THEORIDE_BASE_LIST
+    #undef THEORIDE_CLASS
+    void GenerationMarkerEnd(PublicBase);
 };
+
+//----------------------------------------------------------------------------
+//      基底クラス
+//----------------------------------------------------------------------------
 
 class ProtectedBase
 {
     unsigned int    mUInt;
 public:
     ProtectedBase(unsigned int iUInt) : mUInt(iUInt) { }
+
+    // 自動生成指示
     void GenerationMarkerStart(ProtectedBase, char const* inc="intrusive.inc");
-#define THEORIDE_CLASS ProtectedBase
-#define THEORIDE_BASE_LIST()
-#define THEORIDE_ELEMENT_LIST()\
-    THEORIDE_ELEMENT_PRIVATE(mUInt)
-#include "intrusive.inc"
-#undef THEORIDE_ELEMENT_LIST
-#undef THEORIDE_BASE_LIST
-#undef THEORIDE_CLASS
-void GenerationMarkerEnd(ProtectedBase);
+    #define THEORIDE_CLASS ProtectedBase
+    #define THEORIDE_BASE_LIST()
+    #define THEORIDE_ELEMENT_LIST()\
+        THEORIDE_ELEMENT_PRIVATE(mUInt)
+    #include "intrusive.inc"
+    #undef THEORIDE_ELEMENT_LIST
+    #undef THEORIDE_BASE_LIST
+    #undef THEORIDE_CLASS
+    void GenerationMarkerEnd(ProtectedBase);
 };
+
+//----------------------------------------------------------------------------
+//      基底クラス
+//----------------------------------------------------------------------------
+
 class PrivateBase
 {
     unsigned short  mUShort;
 public:
     PrivateBase(unsigned short iUShort) : mUShort(iUShort) { }
+
+    // 自動生成指示
     void GenerationMarkerStart(PrivateBase, char const* inc="intrusive.inc");
-#define THEORIDE_CLASS PrivateBase
-#define THEORIDE_BASE_LIST()
-#define THEORIDE_ELEMENT_LIST()\
-    THEORIDE_ELEMENT_PRIVATE(mUShort)
-#include "intrusive.inc"
-#undef THEORIDE_ELEMENT_LIST
-#undef THEORIDE_BASE_LIST
-#undef THEORIDE_CLASS
-void GenerationMarkerEnd(PrivateBase);
+    #define THEORIDE_CLASS PrivateBase
+    #define THEORIDE_BASE_LIST()
+    #define THEORIDE_ELEMENT_LIST()\
+        THEORIDE_ELEMENT_PRIVATE(mUShort)
+    #include "intrusive.inc"
+    #undef THEORIDE_ELEMENT_LIST
+    #undef THEORIDE_BASE_LIST
+    #undef THEORIDE_CLASS
+    void GenerationMarkerEnd(PrivateBase);
 };
+
+//----------------------------------------------------------------------------
+//      派生クラス
+//----------------------------------------------------------------------------
 
 class ClassTest : public PublicBase, protected ProtectedBase, private PrivateBase
 {
@@ -93,30 +122,25 @@ public:
         mInt(iInt)
     { }
 
+    // 自動生成指示
     void GenerationMarkerStart(ClassTest, char const* inc="intrusive.inc");
-#define THEORIDE_CLASS ClassTest
-#define THEORIDE_BASE_LIST()\
-    THEORIDE_BASE_PUBLIC(PublicBase)\
-    THEORIDE_BASE_PROTECTED(ProtectedBase)\
-    THEORIDE_BASE_PRIVATE(PrivateBase)
-#define THEORIDE_ELEMENT_LIST()\
-    THEORIDE_ELEMENT_PRIVATE(mEnumTest)\
-    THEORIDE_ELEMENT_PRIVATE(mInt)
-#include "intrusive.inc"
-#undef THEORIDE_ELEMENT_LIST
-#undef THEORIDE_BASE_LIST
-#undef THEORIDE_CLASS
-void GenerationMarkerEnd(ClassTest);
+    #define THEORIDE_CLASS ClassTest
+    #define THEORIDE_BASE_LIST()\
+        THEORIDE_BASE_PUBLIC(PublicBase)\
+        THEORIDE_BASE_PROTECTED(ProtectedBase)\
+        THEORIDE_BASE_PRIVATE(PrivateBase)
+    #define THEORIDE_ELEMENT_LIST()\
+        THEORIDE_ELEMENT_PRIVATE(mEnumTest)\
+        THEORIDE_ELEMENT_PRIVATE(mInt)
+    #include "intrusive.inc"
+    #undef THEORIDE_ELEMENT_LIST
+    #undef THEORIDE_BASE_LIST
+    #undef THEORIDE_CLASS
+    void GenerationMarkerEnd(ClassTest);
 };
 
 int main()
 {
-    EnumTest aEnumTest = EnumTest::Symbol2;
-    std::cout << "aEnumTest = " << aEnumTest << "\n";
-
-    PublicBase   aPublicBase(123);
-    std::cout << "aPublicBase :" << aPublicBase << "\n";
-
     ClassTest   aClassTest(EnumTest::Symbol1, 123);
     std::cout << "aClassTest :" << aClassTest << "\n";
 }
