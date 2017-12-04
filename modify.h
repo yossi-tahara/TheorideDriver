d78f8e01d2b3b5196e90bf6bc39e2312e8d3c697
yossi-tahara <yossi-tahara@theolizer.com>
2017-12-05 01:25:10 +0900

    非侵入型にも対応 modify.hを登録漏れしていた

---
+++ modify.h
@@ -0,0 +1,555 @@
+﻿//############################################################################
+//      ソース修正
+/*
+    © 2017 Theoride Technology (http://theolizer.com/) All Rights Reserved.
+
+    General Public License Version 3 ("GPLv3")
+        You may use this file in accordance with the terms and conditions of 
+        GPLv3 published by Free Software Foundation.
+        Please confirm the contents of GPLv3 at https://www.gnu.org/licenses/gpl.txt .
+        A copy of GPLv3 is also saved in a LICENSE.TXT file.
+
+    General Public License Version 3(以下GPLv3)
+        Free Software Foundationが公表するGPLv3の使用条件に従って、
+        あなたはこのファイルを取り扱うことができます。
+        GPLv3の内容を https://www.gnu.org/licenses/gpl.txt にて確認して下さい。
+        またGPLv3のコピーをLICENSE.TXTファイルにおいてます。
+*/
+//############################################################################
+
+#if !defined(THEOLIZER_MODIFY_H)
+#define THEOLIZER_MODIFY_H
+
+#define ASTANALYZE_OUTPUT(...)
+
+//############################################################################
+//      ソース修正用クラス
+//############################################################################
+
+class ModifySource
+{
+// ***************************************************************************
+//      ユーティリティ
+// ***************************************************************************
+
+private:
+
+//----------------------------------------------------------------------------
+//      文字列の改行を正規化する
+//----------------------------------------------------------------------------
+
+    std::string normalizeLF(std::string&& iString)
+    {
+        if (kLineEnding.equals("\n"))
+    return iString;
+
+        std::string ret;
+        StringRef aString(iString);
+        bool aEndIsLF=aString.endswith(kLineEnding);
+        for (std::pair<StringRef, StringRef> aParsing=aString.split(kLineEnding);
+             true;
+             aParsing=aParsing.second.split(kLineEnding))
+        {
+            ret.append(aParsing.first);
+            if (aParsing.second.empty())
+        break;
+            ret.append("\n");
+        }
+        if (aEndIsLF) ret.append("\n");
+        return ret;
+    }
+
+//----------------------------------------------------------------------------
+//      文字列の改行をOSに合わせる
+//----------------------------------------------------------------------------
+
+    std::string reverseLF(std::string&& iString)
+    {
+        if (kLineEnding.equals("\n"))
+    return iString;
+
+        std::string ret;
+        StringRef aString(iString);
+        bool aEndIsLF=aString.endswith("\n");
+        for (std::pair<StringRef, StringRef> aParsing=aString.split('\n');
+             true;
+             aParsing=aParsing.second.split('\n'))
+        {
+            ret.append(aParsing.first);
+            if (aParsing.second.empty())
+        break;
+            ret.append(kLineEnding);
+        }
+        if (aEndIsLF) ret.append(kLineEnding);
+        return ret;
+    }
+
+//----------------------------------------------------------------------------
+//      指定位置の現ソース獲得
+//----------------------------------------------------------------------------
+
+    std::string getNowSource(SourceLocation iBegin, SourceLocation iEnd)
+    {
+         return normalizeLF(
+            std::move(mRewriter.getRewrittenText(clang::SourceRange(iBegin, iEnd))));
+    }
+
+//----------------------------------------------------------------------------
+//      指定Declの現ソース獲得
+//----------------------------------------------------------------------------
+
+    std::string getDeclSource(Decl const* iDecl)
+    {
+        std::string ret;
+        if (iDecl)
+        {
+            ret = normalizeLF(std::move(mRewriter.getRewrittenText(iDecl->getSourceRange())));
+        }
+        return ret;
+    }
+
+//----------------------------------------------------------------------------
+//      FileID記録
+//----------------------------------------------------------------------------
+
+    void addFileID(const SourceLocation iLocation)
+    {
+        FileID aFileId = gASTContext->getFullLoc(iLocation).getFileID();
+        auto found = std::lower_bound(mModifiedFiles.begin(),
+                                      mModifiedFiles.end(),
+                                      aFileId);
+        if ((found == mModifiedFiles.end()) || (*found != aFileId))
+        {
+            mModifiedFiles.insert(found, aFileId);
+        }
+    }
+
+//----------------------------------------------------------------------------
+//      文字列置き換え
+//----------------------------------------------------------------------------
+
+    void replaceString(SourceLocation iBegin, SourceLocation iEnd, std::string&& iString)
+    {
+        if (mRewriter.ReplaceText(clang::SourceRange(iBegin, iEnd), reverseLF(std::move(iString))))
+        {
+            gCustomDiag.FatalReport(iBegin,
+                "Fatal error. Can not replace source.");
+    return;
+        }
+        addFileID(iBegin);
+    }
+
+//----------------------------------------------------------------------------
+//      文字列挿入
+//----------------------------------------------------------------------------
+
+    void instertString(SourceLocation iLocation, std::string&& iString)
+    {
+        if (mRewriter.InsertText(iLocation, reverseLF(std::move(iString))))
+        {
+            gCustomDiag.FatalReport(iLocation,
+                "Fatal error. Can not replace source.");
+    return;
+        }
+        addFileID(iLocation);
+    }
+
+//----------------------------------------------------------------------------
+//      名前空間取り出し
+//----------------------------------------------------------------------------
+
+class NameSpaces
+{
+    struct NamespaceInfo
+    {
+        bool        mIsInline;
+        StringRef   mName;
+        NamespaceInfo(bool iIsInline, StringRef iName) :
+            mIsInline(iIsInline), mName(iName)
+        { }
+    };
+    typedef SmallVector<NamespaceInfo, 8>           Vector;
+    typedef typename Vector::const_reverse_iterator Iterator;
+
+    Vector  mVector;
+
+public:
+    NameSpaces() { }
+    NameSpaces(NamedDecl const* iNamedDecl, bool iSuppressUnwrittenScope=true)
+    {
+        clang::DeclContext const* aContext = iNamedDecl->getDeclContext();
+        while (aContext && clang::isa<NamedDecl>(aContext))
+        {
+            clang::NamespaceDecl const* nsd = dyn_cast<clang::NamespaceDecl>(aContext);
+            if (nsd)
+            {
+                if (iSuppressUnwrittenScope)
+                {
+                    if (!(nsd->isAnonymousNamespace()) && (!(nsd->isInline())))
+                        mVector.push_back(NamespaceInfo(false, nsd->getName()));
+                }
+                else
+                {
+                    if (nsd->isAnonymousNamespace())
+                    {
+                        mVector.push_back(NamespaceInfo(false, StringRef("")));
+                    }
+                    else
+                    {
+                        mVector.push_back(NamespaceInfo(nsd->isInline(), nsd->getName()));
+                    }
+                }
+            }
+            aContext = aContext->getParent();
+        }
+    }
+
+    Iterator begin() {return mVector.rbegin();}
+    Iterator end()   {return mVector.rend();}
+};
+
+//----------------------------------------------------------------------------
+//      名前空間付きの名前獲得
+//----------------------------------------------------------------------------
+
+std::string getQualifiedName(NamedDecl const* iNamedDecl, bool iSuppressUnwrittenScope=true)
+{
+    std::string ret;
+    for (auto aNameSpace : NameSpaces(iNamedDecl, iSuppressUnwrittenScope))
+    {
+        ret.append(aNameSpace.mName.str());
+        ret.append("::");
+    }
+    ret.append(iNamedDecl->getName().str());
+
+    return ret;
+}
+
+//----------------------------------------------------------------------------
+//      マーカ用FunctionDeclの終わり(;の次の位置)を探す
+//          ;までの間にスペースとタブ以外の文字があるとエラーとする。
+//          改行も不許可とする。
+//----------------------------------------------------------------------------
+
+    SourceLocation getMarkerEnd(SourceLocation iStartLoc)
+    {
+        FullSourceLoc aLoc =
+            gASTContext->getFullLoc(iStartLoc).getSpellingLoc();
+llvm::outs() << "aLoc=" << aLoc.printToString(*gSourceManager) << "\n";
+
+        // ;まで探す
+        FileID aFileId=aLoc.getFileID();
+        unsigned aLine=aLoc.getSpellingLineNumber();
+        unsigned aCol =aLoc.getSpellingColumnNumber()+1;
+        for (char const* p = gSourceManager->getCharacterData(aLoc)+1;
+             *p != ';';
+             ++p, ++aCol)
+        {
+            if ((*p != ' ') && (*p != '\t'))
+            {
+                SourceLocation aErrLoc=gSourceManager->translateLineCol(aFileId, aLine, aCol);
+                gCustomDiag.ErrorReport(aErrLoc, "No semicolon.");
+    return FullSourceLoc();
+            }
+        }
+        ++aCol;
+
+        return gSourceManager->translateLineCol(aFileId, aLine, aCol);
+    }
+
+// ***************************************************************************
+//      ソース修正
+// ***************************************************************************
+
+    void modifySource
+    (
+        clang::FunctionDecl* iStartMarker,
+        clang::TagDecl* iTarget,
+        std::string&& iString
+    )
+    {
+        SourceLocation aStartLoc = getMarkerEnd(iStartMarker->getLocEnd());
+llvm::outs() << "aStartLoc=" << aStartLoc.printToString(*gSourceManager) << "\n";
+
+        auto aEnd = mAstInterface.mEndMarkerList.find(iTarget);
+        // 終了マーカなし
+        if (aEnd == mAstInterface.mEndMarkerList.end())
+        {
+            instertString(aStartLoc, std::move(iString));
+        }
+
+        // 終了マーカあり
+        else
+        {
+            SourceLocation aEndLoc = getMarkerEnd(aEnd->second);
+llvm::outs() << "---------------------------------------- getNowSource(enum)";
+llvm::outs() << getNowSource(aStartLoc, aEndLoc);
+llvm::outs() << "\n----------------------------------------\n";
+llvm::outs() << "    " << aEnd->second.printToString(*gSourceManager) << "\n";
+llvm::outs() << "    " << aStartLoc.printToString(*gSourceManager) << "\n";
+llvm::outs() << "    " << aEndLoc.printToString(*gSourceManager) << "\n";
+            // 変更があれば修正する
+            if (getNowSource(aStartLoc, aEndLoc) != iString)
+            {
+llvm::outs() << "    changed !! \n";
+                replaceString(aStartLoc, aEndLoc, std::move(iString));
+            }
+        }
+    }
+
+// ***************************************************************************
+//      enum用ソース生成
+//          #define THEORIDE_ENUM       enum名(名前空間付き)
+//          #define THEORIDE_ENUM_LIST()\
+//              THEORIDE_ELEMENT(シンボル名)\
+//              THEORIDE_ELEMENT(シンボル名)\
+//              THEORIDE_ELEMENT(シンボル名)\
+//                  :
+//          #include "指定ファイル名"
+//          #undef THEORIDE_ENUM_LIST
+//          #undef THEORIDE_ENUM
+//          void GenerationMarkerEnd(enum名);
+// ***************************************************************************
+
+public:
+    void createEnum
+    (
+        clang::EnumDecl*        iTargetEnum,
+        std::string const&      iIncFileName,
+        clang::FunctionDecl*    iStartMarker
+    )
+    {
+        std::string aEnumName = getQualifiedName(iTargetEnum);
+
+        std::stringstream   aSource;
+        aSource << "\n#define THEORIDE_ENUM " << aEnumName << "\n";
+
+        // シンボル・リスト生成
+        aSource << "#define THEORIDE_ENUM_LIST()";
+        for (auto symbol : iTargetEnum->enumerators())
+        {
+            aSource << "\\\n"
+                << "    THEORIDE_ELEMENT(" << symbol->getName().str() << ")";
+        }
+        aSource << "\n";
+
+        aSource << "#include \"" << iIncFileName << "\"\n";
+        aSource << "#undef THEORIDE_ENUM_LIST\n";
+        aSource << "#undef THEORIDE_ENUM\n";
+        aSource << "void GenerationMarkerEnd(" << aEnumName << ");";
+
+llvm::outs() << "---------------------------------------- enum";
+llvm::outs() << aSource.str();
+llvm::outs() << "\n----------------------------------------\n";
+
+        modifySource(iStartMarker, iTargetEnum, aSource.str());
+
+llvm::outs().flush();
+    }
+
+// ***************************************************************************
+//      class用ソース生成
+//          #define THEORIDE_CLASS      class名(名前空間付き)
+//          #define THEORIDE_BASE_LIST()\
+//              THEORIDE_BASE_PUBLIC(基底クラス名)\
+//              THEORIDE_BASE_PROTECTED(基底クラス名)\
+//              THEORIDE_BASE_PRIVATE(基底クラス名)\
+//                  :
+//          #define THEORIDE_ELEMENT_LIST()\
+//              THEORIDE_ELEMENT_PUBLIC(変数名)\
+//              THEORIDE_ELEMENT_PROTECTED(変数名)\
+//              THEORIDE_ELEMENT_PRIVATE(変数名)\
+//                  :
+//          #include "指定ファイル名"
+//          #undef THEORIDE_ELEMENT_LIST
+//          #undef THEORIDE_BASE_LIST
+//          #undef THEORIDE_CLASS
+//          void GenerationMarkerEnd(class名);
+// ***************************************************************************
+
+    void createClass
+    (
+        clang::CXXRecordDecl*   iTargetClass,
+        std::string const&      iIncFileName,
+        clang::FunctionDecl*    iStartMarker
+    )
+    {
+
+        SourceLocation aBegin = iStartMarker->getSourceRange().getBegin();
+llvm::outs() << "    iStartMarker : " << aBegin.printToString(*gSourceManager) << "\n";
+        FullSourceLoc aLoc =
+            gASTContext->getFullLoc(aBegin).getSpellingLoc();
+llvm::outs() << "aLoc=" << aLoc.printToString(*gSourceManager) << "\n";
+        unsigned indent =aLoc.getSpellingColumnNumber()-1;
+llvm::outs() << "indent=" << indent << "\n";
+        std::string aIndent(indent, ' ');
+
+        std::string aClassName = getQualifiedName(iTargetClass);
+
+        std::stringstream   aSource;
+        aSource << "\n" << aIndent << "#define THEORIDE_CLASS " << aClassName;
+
+        // ベース・リスト生成
+        aSource << "\n" << aIndent << "#define THEORIDE_BASE_LIST()";
+        for (auto& base : iTargetClass->bases())
+        {
+            QualType qt = base.getType().getCanonicalType();
+            CXXRecordDecl const* aBase = qt->getAsCXXRecordDecl();
+            if (!aBase)
+        continue;
+
+            std::string aBaseName = qt.getCanonicalType().getAsString(mPrintingPolicy);
+            switch(base.getAccessSpecifier())
+            {
+            case clang::AS_public:
+                aSource << "\\\n" << aIndent << "    THEORIDE_BASE_PUBLIC(" << aBaseName << ")";
+                break;
+
+            case clang::AS_protected:
+                aSource << "\\\n" << aIndent << "    THEORIDE_BASE_PROTECTED(" << aBaseName << ")";
+                break;
+
+            case clang::AS_private:
+                aSource << "\\\n" << aIndent << "    THEORIDE_BASE_PRIVATE(" << aBaseName << ")";
+                break;
+            }
+        }
+
+        // メンバ変数リスト生成
+        aSource << "\n" << aIndent << "#define THEORIDE_ELEMENT_LIST()";
+        for (auto field : iTargetClass->fields())
+        {
+            std::string aElementName = field->getName();
+            switch(field->getAccess())
+            {
+            case clang::AS_public:
+                aSource<<"\\\n"<<aIndent<<"    THEORIDE_ELEMENT_PUBLIC("<< aElementName<<")";
+                break;
+
+            case clang::AS_protected:
+                aSource<<"\\\n"<<aIndent<<"    THEORIDE_ELEMENT_PROTECTED("<<aElementName<<")";
+                break;
+
+            case clang::AS_private:
+                aSource<<"\\\n"<<aIndent<<"    THEORIDE_ELEMENT_PRIVATE("<<aElementName<<")";
+                break;
+            }
+        }
+
+        aSource << "\n" << aIndent << "#include \"" << iIncFileName << "\"";
+        aSource << "\n" << aIndent << "#undef THEORIDE_ELEMENT_LIST";
+        aSource << "\n" << aIndent << "#undef THEORIDE_BASE_LIST";
+        aSource << "\n" << aIndent << "#undef THEORIDE_CLASS";
+        aSource << "\n" << aIndent << "void GenerationMarkerEnd(" << aClassName << ");";
+
+llvm::outs() << "---------------------------------------- class";
+llvm::outs() << aSource.str();
+llvm::outs() << "\n----------------------------------------\n";
+
+        modifySource(iStartMarker, iTargetClass, aSource.str());
+
+llvm::outs().flush();
+    }
+
+// ***************************************************************************
+//      ソース保存
+// ***************************************************************************
+
+    void saveSources()
+    {
+//          ---<<< ソース保存 >>>---
+
+        ASTANALYZE_OUTPUT("mModifiedFiles.size()=", mModifiedFiles.size());
+
+        // 致命的エラーが発生していたら保存しない
+        if (gCustomDiag.getDiags().hasFatalErrorOccurred())
+        {
+            ASTANALYZE_OUTPUT("HandleTranslationUnit() aborted by Fatal Error.");
+    return;
+        }
+        // Theolizerがエラー検出していたら保存しない
+        if (gCustomDiag.hasErrorOccurred())
+        {
+            ASTANALYZE_OUTPUT("HandleTranslationUnit() aborted by Error.");
+    return;
+        }
+
+        // 修正ファイルがないなら保存しない
+        if (!mModifiedFiles.size())
+    return;
+
+//      ---<<< 保存処理 >>>---
+
+        // 修正開始してみる。他が修正していたら、再処理する
+        boostI::upgradable_lock<ExclusiveControl> lock_try(*gExclusiveControl,boostI::try_to_lock);
+        ASTANALYZE_OUTPUT("pre lock_try");
+        if (!lock_try)
+        {
+            ASTANALYZE_OUTPUT("post lock_try(1)");
+    return;
+        }
+        ASTANALYZE_OUTPUT("post lock_try(2)");
+
+        // 他のプロセスのファイル開放待ち
+        boostI::scoped_lock<ExclusiveControl> lock(*gExclusiveControl);
+
+        for (auto aFileId2 : mModifiedFiles)
+        {
+            const FileEntry* file = gSourceManager->getFileEntryForID(aFileId2);
+            ASTANALYZE_OUTPUT(file->getName(), " ==============================================\n");
+            std::string file_data;
+            llvm::raw_string_ostream ss(file_data);
+            mRewriter.getEditBuffer(aFileId2).write(ss);
+            ss.flush();
+            std::string file_body=normalizeLF(std::move(file_data));
+            ASTANALYZE_OUTPUT(file_body);
+            ASTANALYZE_OUTPUT("==============================================");
+        }
+
+        // 保存処理
+        bool error=mRewriter.overwriteChangedFiles();
+        ASTANALYZE_OUTPUT("overwriteChangedFiles()=", error);
+
+        // 保存処理を実行したら、mRetryAST(最新版変更によるリトライ)をgRetryASTへ反映
+        gRetryAST = mRetryAST;
+        ASTANALYZE_OUTPUT("gRetryAST=", gRetryAST);
+    }
+
+// ***************************************************************************
+//      生成と削除
+// ***************************************************************************
+
+private:
+    // 入力メンバ
+    AstInterface&           mAstInterface;          // AST解析処理とのI/F
+    clang::PrintingPolicy const& mPrintingPolicy;
+
+    // 処理用メンバ
+    Rewriter                mRewriter;              // ソース修正用
+    std::vector<FileID>     mModifiedFiles;         // 修正したソース
+
+    std::stringstream       mLastVersion;           // 最新版マクロ定義ソース
+
+    int                     mNamespaceNestCount;    // 名前空間のネスト数
+    NamedDecl const*        mTarget;                // 処理中のターゲット(エラー表示用)
+
+    bool                    mRetryAST;              // AST処理再実行要求(最新版を変更した時)
+
+public:
+    ModifySource(AstInterface& iAstInterface) :
+        mAstInterface(iAstInterface),
+        mPrintingPolicy(gASTContext->getPrintingPolicy()),
+        mRetryAST(false)
+
+    {
+        // リライター準備
+        mRewriter.setSourceMgr(*gSourceManager, gASTContext->getLangOpts());
+        mModifiedFiles.clear();
+    }
+
+    ~ModifySource()
+    {
+    }
+};
+
+#endif
--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,4 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
 #define THEORIDE_DO_PROCESS
 
 //----------------------------------------------------------------------------
@@ -37,110 +38,95 @@ void GenerationMarkerStart(EnumTest, char const* inc="enum.inc");
 void GenerationMarkerEnd(EnumTest);
 
 //----------------------------------------------------------------------------
-//      基底クラス
+//      非侵入型
 //----------------------------------------------------------------------------
 
-class PublicBase
-{
-    unsigned long   mULong;
-public:
-    PublicBase(unsigned long iULong) : mULong(iULong) { }
+#include <ctime>
 
-    // 自動生成指示
-    void GenerationMarkerStart(PublicBase, char const* inc="intrusive.inc");
-    #define THEORIDE_CLASS PublicBase
-    #define THEORIDE_BASE_LIST()
-    #define THEORIDE_ELEMENT_LIST()\
-        THEORIDE_ELEMENT_PRIVATE(mULong)
-    #include "intrusive.inc"
-    #undef THEORIDE_ELEMENT_LIST
-    #undef THEORIDE_BASE_LIST
-    #undef THEORIDE_CLASS
-    void GenerationMarkerEnd(PublicBase);
-};
-
-//----------------------------------------------------------------------------
-//      基底クラス
-//----------------------------------------------------------------------------
-
-class ProtectedBase
-{
-    unsigned int    mUInt;
-public:
-    ProtectedBase(unsigned int iUInt) : mUInt(iUInt) { }
-
-    // 自動生成指示
-    void GenerationMarkerStart(ProtectedBase, char const* inc="intrusive.inc");
-    #define THEORIDE_CLASS ProtectedBase
-    #define THEORIDE_BASE_LIST()
-    #define THEORIDE_ELEMENT_LIST()\
-        THEORIDE_ELEMENT_PRIVATE(mUInt)
-    #include "intrusive.inc"
-    #undef THEORIDE_ELEMENT_LIST
-    #undef THEORIDE_BASE_LIST
-    #undef THEORIDE_CLASS
-    void GenerationMarkerEnd(ProtectedBase);
-};
+// 自動生成指示
+void GenerationMarkerStart(tm, char const* inc="non_intrusive.inc");
+#define THEORIDE_CLASS tm
+#define THEORIDE_BASE_LIST()
+#define THEORIDE_ELEMENT_LIST()\
+    THEORIDE_ELEMENT_PUBLIC(tm_sec)\
+    THEORIDE_ELEMENT_PUBLIC(tm_min)\
+    THEORIDE_ELEMENT_PUBLIC(tm_hour)\
+    THEORIDE_ELEMENT_PUBLIC(tm_mday)\
+    THEORIDE_ELEMENT_PUBLIC(tm_mon)\
+    THEORIDE_ELEMENT_PUBLIC(tm_year)\
+    THEORIDE_ELEMENT_PUBLIC(tm_wday)\
+    THEORIDE_ELEMENT_PUBLIC(tm_yday)\
+    THEORIDE_ELEMENT_PUBLIC(tm_isdst)
+#include "non_intrusive.inc"
+#undef THEORIDE_ELEMENT_LIST
+#undef THEORIDE_BASE_LIST
+#undef THEORIDE_CLASS
+void GenerationMarkerEnd(tm);
 
 //----------------------------------------------------------------------------
 //      基底クラス
 //----------------------------------------------------------------------------
 
-class PrivateBase
+class Base
 {
-    unsigned short  mUShort;
+    int     mInt;
 public:
-    PrivateBase(unsigned short iUShort) : mUShort(iUShort) { }
+    Base(int iInt) : mInt(iInt) { }
 
     // 自動生成指示
-    void GenerationMarkerStart(PrivateBase, char const* inc="intrusive.inc");
-    #define THEORIDE_CLASS PrivateBase
+    void GenerationMarkerStart(Base, char const* inc="intrusive.inc");
+    #define THEORIDE_CLASS Base
     #define THEORIDE_BASE_LIST()
     #define THEORIDE_ELEMENT_LIST()\
-        THEORIDE_ELEMENT_PRIVATE(mUShort)
+        THEORIDE_ELEMENT_PRIVATE(mInt)
     #include "intrusive.inc"
     #undef THEORIDE_ELEMENT_LIST
     #undef THEORIDE_BASE_LIST
     #undef THEORIDE_CLASS
-    void GenerationMarkerEnd(PrivateBase);
+    void GenerationMarkerEnd(Base);
 };
 
 //----------------------------------------------------------------------------
 //      派生クラス
 //----------------------------------------------------------------------------
 
-class ClassTest : public PublicBase, protected ProtectedBase, private PrivateBase
+class Derived : public Base
 {
-    EnumTest    mEnumTest;
-    int         mInt;
+    tm          mTm;
+    EnumTest    mPrivate;
+protected:
+    int         mProtected;
 public:
-    ClassTest(EnumTest iEnumTest, int iInt) :
-        PublicBase(iInt*1000),
-        ProtectedBase(iInt*100),
-        PrivateBase(static_cast<unsigned short>(iInt*10)),
-        mEnumTest(iEnumTest),
-        mInt(iInt)
-    { }
+    long        mPublic;
+    Derived(EnumTest iEnumTest, int iData) :
+        Base(iData*100),
+        mPrivate(iEnumTest),
+        mProtected(iData*10),
+        mPublic(iData)
+    {
+        time_t  now = time(nullptr);
+        mTm = *localtime(&now);
+    }
 
     // 自動生成指示
-    void GenerationMarkerStart(ClassTest, char const* inc="intrusive.inc");
-    #define THEORIDE_CLASS ClassTest
+    void GenerationMarkerStart(Derived, char const* inc="intrusive.inc");
+    #define THEORIDE_CLASS Derived
     #define THEORIDE_BASE_LIST()\
-        THEORIDE_BASE_PUBLIC(PublicBase)\
-        THEORIDE_BASE_PROTECTED(ProtectedBase)\
-        THEORIDE_BASE_PRIVATE(PrivateBase)
+        THEORIDE_BASE_PUBLIC(Base)
     #define THEORIDE_ELEMENT_LIST()\
-        THEORIDE_ELEMENT_PRIVATE(mEnumTest)\
-        THEORIDE_ELEMENT_PRIVATE(mInt)
+        THEORIDE_ELEMENT_PRIVATE(mTm)\
+        THEORIDE_ELEMENT_PRIVATE(mPrivate)\
+        THEORIDE_ELEMENT_PROTECTED(mProtected)\
+        THEORIDE_ELEMENT_PUBLIC(mPublic)
     #include "intrusive.inc"
     #undef THEORIDE_ELEMENT_LIST
     #undef THEORIDE_BASE_LIST
     #undef THEORIDE_CLASS
-    void GenerationMarkerEnd(ClassTest);
+    void GenerationMarkerEnd(Derived);
 };
 
 int main()
 {
-    ClassTest   aClassTest(EnumTest::Symbol1, 123);
-    std::cout << "aClassTest :" << aClassTest << "\n";
+    Derived   aDerived(EnumTest::Symbol1, 123);
+    std::cout << "aDerived :" << aDerived << "\n";
 }
