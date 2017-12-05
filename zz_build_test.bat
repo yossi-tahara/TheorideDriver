e9222609ca7c4e0096e559a43c5aba785ce83f7a
yossi-tahara <yossi-tahara@theolizer.com>
2017-12-05 14:05:18 +0900

    private継承も派生クラスでアクセスできるので修正 バッチのchcpをコメントアウト(これがないと文字化けすることがあるが、あるとビルド結果が見れない)

--- zz_build_test.bat
+++ zz_build_test.bat
@@ -3,7 +3,7 @@ cmake --build . --config Release --target Install
 
 if %ERRORLEVEL% == 0 (
     call "N:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-    chcp 65001
+rem chcp 65001
     ctest -C Release -V
 )
 pause
